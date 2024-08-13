9330fb7e6aa141faa7282df8cf9d15b822d2f8fc
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-14 02:16:03 +0530

    db connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require('dotenv');
-const colors = require('colors');
+const colors = require('colors'); 
 const morgan = require('morgan');
 const cors = require('cors');
 
