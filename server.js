0d9220f743ca4617b9e3b79bf342c71e5d578f5f
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-13 22:32:18 +0530

    server initiated

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+// rest object
+const app = express();
+
+// routes
+// 1 test route
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:"Welcome to Blood Bank App"
+    });
+})
+// port 
+const PORT = 8080;
+// listen
+app.listen(PORT,()=>{
+    console.log("Node server Runnign");
+})
