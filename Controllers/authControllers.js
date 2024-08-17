eb63c40517f52387076cf0a7af5841ccafd9c9c3
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-17 13:35:10 +0530

    authoContoller created

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/userModel')
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const registerController = async(req,res)=>{
     try{
 
@@ -23,7 +23,7 @@ const registerController = async(req,res)=>{
             success:true,
             message:'User Registered Successfully'
             ,user,
-            
+
 
         });
     }
@@ -39,4 +39,73 @@ const registerController = async(req,res)=>{
 
 
 };
-module.exports = {registerController}
+// login call back
+const loginController = async (req,res) =>{
+    try{
+        const user = await userModel.findOne({email:req.body.email})
+        if(!user)
+        {
+            return res.status(404).send({
+                success:false,
+                message:'Invalid Credentials'
+            })
+        }
+        // compare password
+        const comparePassword = await bcrypt.compare(req.body.password,user.password);
+        if(!comparePassword)
+        {
+            return res.status(500).send({
+                success:false,
+                message:'Invalid Credentials'
+            });
+
+        }
+        const token = jwt.sign({ userId: user._id },process.env.JWT_SECRET, {
+            expiresIn:'1d',
+        })
+
+        return res.status(200).send({
+            success:true,
+            message:'Login Successfully',
+            token,
+            user,
+        })
+
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in login API',
+            error
+        })
+
+    }
+};
+
+// get current user
+const currentUserController = async(req,res) =>{
+    try{
+
+        const user = await userModel.findOne({_id:req.body.userId})
+        return res.status(200).send({
+            success:true,
+            message:'User fetched Successfully',
+            user
+        })
+    }
+    catch(error)
+    {
+        console.log(error)
+        return res.status(500).send({
+            success:false,
+            message:'unable to get current user',
+            error
+        })
+
+    }
+
+
+}
+
+module.exports = {registerController, loginController, currentUserController}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const { registerController } = require('../Controllers/authControllers');
+const { 
+    registerController,
+    loginController,
+    currentUserController
+ } = require('../Controllers/authControllers');
+const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 // routes
-
+// register || post
 router.post('/register',registerController)
+// login || post
+router.post('/login',loginController)
+// get current user }|| get
+
+router.get('/current-user',authMiddleware,currentUserController)
 module.exports = router
