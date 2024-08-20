6630d3a3b80082263d6e8f2155ffc51a02473ba8
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-20 19:36:15 +0530

    frontend started

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Blood Bridge app</h1>
+
+      
+    </div>
+  );
+}
+
+export default App;
