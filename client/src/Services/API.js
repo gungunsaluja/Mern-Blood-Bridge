a25a0aa1d6e1f54fd6b2600a2ade41ed6f81158e
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-23 23:47:27 +0530

    concurrent package added

--- client/src/Services/API.js
+++ client/src/Services/API.js
@@ -1,1 +1,15 @@
 import axios from `axios`
+
+const API = axios.create({baseURL:process.env.REACT_APP_BASEURL})
+
+API.interceptors.request.use((req)=>{
+    if(localStorage.getItem('token'))
+    {
+        req.headers.Authorization = `Bearer ${localStorage.getItem(`tekon`)}`
+
+    }
+    return req;
+
+})
+
+export default API;
