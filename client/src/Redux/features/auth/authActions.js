addc497cafd4d045324b2f275687ebb843a0310e
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-24 00:54:12 +0530

    import toaster

---
+++ client/src/Redux/features/auth/authActions.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const userLogin = createAsyncThunk(
+    'auth/login',
+    async({role,email,password},{rejectWithValue})=>{
+        try{
+            const{data} = await API.post('/auth.login',{role,email,password})
+            // store token
+            if(data.success)
+            {
+                localStorage.setItem('token',data.token)
+            }
+
+        }
+        catch(error)
+        {
+            if(error.response && error.response.data.message)
+            {
+                return rejectWithValue(error.response.data.message)  
+            }
+            else{
+                return rejectWithValue(error.mesaa
+
+                )
+            }
+        }
+
+    }
+)
