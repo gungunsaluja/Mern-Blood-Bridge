107fc286403cf499d0dd65e4bf4bd841bd888481
gungunsaluja <gungunsaluja16@gmail.com>
2024-08-28 21:12:24 +0530

    table from bootstrap

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import React,{useEffect,useState} from "react";
 
-import { useSelector } from 'react-redux'
+import { useSelector,useEffect } from 'react-redux'
 import Spinner from '../Component/shared/Spinner';
 import Layout from '../Component/shared/Layout/Layout';
 import Model from '../Component/shared/Model/Model';
 
 const HomePage = () => {
   const {loading,error} = useSelector(state=>state.auth);
-  const {data,setData} = useState()
+  const {data,setData} = useState([]);
   // get function
   const getBloodRecords = async()=>{
     try{
@@ -16,15 +16,19 @@ const HomePage = () => {
       const {data} = await API.get('/inventory/get-inventory')
       if(data?.success)
       {
-        setData(data?.inventory)
+        setData(data?.inventory);
 
       }
-    }
-
+    } 
     catch(error)
     {
       console.log(error);
     }
+
+    useEffect(()=>{
+      getBloodRecords();
+    },[]);
+   
   }
 
   return (
@@ -37,7 +41,40 @@ const HomePage = () => {
       data-bs-toggle = "model"
       data-bs-target = "#staticBackdrop"
       >
-        <i className = 'fa-solid fa-plus text-success py-4'></i>Add Inventory</h4>
+        <i className = 'fa-solid fa-plus text-success py-4'></i>
+        
+
+        Add Inventory</h4>
+      <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colSpan={2}>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
         <Model></Model></>)}
       
       
